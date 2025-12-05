d04d44a1d92a975d3376b16c225c835bb44c19d2
Laraib007 <lrbkhan01@gmail.com>
2025-12-05 22:18:28 +0500

    installing flowbiteCSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Laraib Khan
+     
+<form class="max-w-sm mx-auto space-y-4">
+    <div>
+        <label for="visitors" class="block mb-2.5 text-sm font-medium text-heading">Small Input</label>
+        <input type="text" id="visitors" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body" placeholder="" required />
+    </div>
+        <div>
+        <label for="visitors" class="block mb-2.5 text-sm font-medium text-heading">Extra Large Input</label>
+        <input type="text" id="visitors" class="bg-neutral-secondary-medium border border-default-medium text-heading text-base rounded-base focus:ring-brand focus:border-brand block w-full px-4 py-3.5 shadow-xs placeholder:text-body" placeholder="" required />
+    </div>
+</form>
+
     </div>
   );
 }
