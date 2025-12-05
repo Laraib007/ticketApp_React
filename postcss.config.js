3c9c0ee6789d09cca8cca0c837a8e2a150fb40e3
Laraib007 <lrbkhan01@gmail.com>
2025-12-05 23:55:27 +0500

    fixing pkgs

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'), // pehle yaha require('tailwindcss') tha
+    require('autoprefixer'),
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/*.js", // apne project ke files ke paths daalein
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
