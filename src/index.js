21456ca26c4658df3be918de87455f982ee321e2
Laraib007 <lrbkhan01@gmail.com>
2025-12-06 22:55:10 +0500

    setting routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tickets from './tickets';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/tickets" element={<Tickets/>} />
+    </Routes> 
+    </BrowserRouter>  
   </React.StrictMode>
 );
 
