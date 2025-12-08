a7a1cd1a63eb8d844aa519e0799063c866daee95
Laraib007 <lrbkhan01@gmail.com>
2025-12-08 22:22:37 +0500

    validation email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 // Creating a New User
 router.post('/create', async (req, res) => {
-   
-
-    let user = await users.create({
+   let user = users.findOne({email: req.body.email})
+if(user){
+    res.send("User Aleady Exist") 
+}
+     user = await users.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
