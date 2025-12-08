b2a21e48453676d95cd98219bf1864af4703b32f
Laraib007 <lrbkhan01@gmail.com>
2025-12-08 22:36:24 +0500

    coding

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,16 +4,18 @@ const router = express.Router();
 
 // Creating a New User
 router.post('/create', async (req, res) => {
-   let user = users.findOne({email: req.body.email})
+   let user = await users.findOne({email: req.body.email})
 if(user){
-    res.send("User Aleady Exist") 
-}
+    res.status(404).send("User Aleady Exist") 
+    return
+} else {
      user = await users.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     })
     res.send(user );
+    }
 });
 
 
