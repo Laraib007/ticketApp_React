0a27e8cc827dbdad3281a2292ea63872911ef1dd
Laraib007 <lrbkhan01@gmail.com>
2025-12-12 22:36:45 +0500

    making ticket schema

---
+++ backend/routes/ticket.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const tickets = require('../modules/tickets');
+
+// Creating a New Ticket
+router.post('/create', async (req, res) => {
+
+     let ticket = await tickets.create( {
+        ticket: req.body.ticket,
+        desc: req.body.desc,
+    })
+    res.json({"Ticket": ticket} );
+    return
+});
+
+
+
+
+module.exports = router;
