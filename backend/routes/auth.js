410cde7ed847b487d2aaf1a5b88081209d4ba26d
Laraib007 <lrbkhan01@gmail.com>
2025-12-12 21:32:55 +0500

    alhamulillah work

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,11 +15,44 @@ if(user){
         email: req.body.email,
         password: req.body.password
     })
-    let token = await jwt.sign({user}, 'shhhhh');
+    let token = await jwt.sign(user.email, 'shhhhh');
     res.json({"jwtToken": token} );
     }
 });
 
+// eyJhbGciOiJIUzI1NiJ9.bGFyYWFhYWJAZXFtYWlhcWFsLmNvbXE.BtlF8JGDFPidYk1jpEHK-5PuXwqrXtiaB-Q6EMXG_Sw
+
+router.post('/login', async (req, res) => {
+   let user = await users.findOne({email: req.body.email})
+if(!user){
+    res.status(404).send("User Not Exist") 
+    return
+} else {
+    //  user = await users.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+    let token = await jwt.sign(user.email, 'shhhhh');
+    res.json({"jwtToken": token} );
+    }
+});
+
+router.post('/validate', async (req, res) => {
+ let user = await {token: req.body.token}
+    try {
+    
+let dToken = await jwt.verify(user.token, 'shhhhh');
+res.json({"email": dToken})
+    } catch {
+              
+        res.send("Some Error")
+        return
+    }
+    
+
+ 
+});
 
 
 
